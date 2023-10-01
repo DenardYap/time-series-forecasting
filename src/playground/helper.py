6595e454bf9a9ea60204953b42ccaf1e0aa0c56b
DenardYap <mameehy@hotmail.com>
2023-10-01 01:27:10 -0400

    Added codes for electricity dataset and added CRPS metric

---
+++ src/playground/helper.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def CRPS(forecast, observed):
+    # Sort the forecast and observed values
+    forecast_sorted = np.sort(forecast)
+    observed_sorted = np.sort(observed)
+    
+    # Create an array of evenly spaced quantiles
+    quantiles = np.linspace(0, 1, len(forecast))
+    
+    # Calculate the differences between quantiles
+    diff = forecast_sorted - observed_sorted
+    
+    # Calculate the CRPS
+    crps = np.trapz(np.abs(diff), quantiles)
+    
+    return crps
+
+
+def crps_nrg(y_true, y_pred, sample_weight=None):
+    num_samples = y_pred.shape[0]
+    absolute_error = np.mean(np.abs(y_pred - y_true), axis=0)
+
+    if num_samples == 1:
+        return np.average(absolute_error, weights=sample_weight)
+
+    y_pred = np.sort(y_pred, axis=0)
+    diff = y_pred[1:] - y_pred[:-1]
+    weight = np.arange(1, num_samples) * np.arange(num_samples - 1, 0, -1)
+    weight = np.expand_dims(weight, -1)
+
+    per_obs_crps = absolute_error - np.sum(diff * weight, axis=0) / num_samples**2
+    return np.average(per_obs_crps, weights=sample_weight)
+
+
+def crps_pwm(y_true, y_pred, sample_weight=None):
+    num_samples = y_pred.shape[0]
+    absolute_error = np.mean(np.abs(y_pred - y_true), axis=0)
+
+    if num_samples == 1:
+        return np.average(absolute_error, weights=sample_weight)
+
+    y_pred = np.sort(y_pred, axis=0)
+    b0 = y_pred.mean(axis=0)
+    b1_values = y_pred * np.arange(num_samples).reshape((num_samples, 1))
+    b1 = b1_values.mean(axis=0) / num_samples
+
+    per_obs_crps = absolute_error + b0 - 2 * b1
+    return np.average(per_obs_crps, weights=sample_weight)
